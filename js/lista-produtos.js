194f09a5d8c298c75bfe653f5e6d525ed8743d9e
unamednada <dias.gustavo.dias@gmail.com>
2021-08-24 09:31:54 -0300

    Create createproductitem and test succesfully

--- js/lista-produtos.js
+++ js/lista-produtos.js
@@ -1,16 +1,31 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const url = 'https://api.mercadolibre.com/sites/MLB/search?q=camisas';
+const productList = document.querySelector('#listagem');
 
-const fetchAllTshirts = async () => {
+
+const fetchResultArray = async () => {
 
   try {
-    const response = fetch(url);
-    const jsonResponse = response.json();
-    return jsonResponse;
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    return jsonResponse.results;
   } catch (error) {
     console.log(error);
   }
 }
 
+fetchResultArray().then((resultArray) => console.log(resultArray));
 
+const createProductItem = ({ price, thumbnail, title }) => {
+  const item = document.createElement('div');
+  item.innerHTML = `
+  <img src=${thumbnail} />
+  <p>${title}</p>
+  <p>R$ ${price}`;
+  productList.appendChild(item);
+}
+
+fetchResultArray().then((resultArray) => {
+  resultArray.forEach((result) => createProductItem(result));
+})
